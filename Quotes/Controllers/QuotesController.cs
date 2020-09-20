e2ed89e18ea51ad2e3732025ff0e965c417e86f0
Siyovush <siyovush@go.alif.academy>
2020-09-20 22:22:51 +0500

    Add CRUD operations for Quotes

--- Quotes/Controllers/QuotesController.cs
+++ Quotes/Controllers/QuotesController.cs
@@ -3,43 +3,135 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Quotes.Models;
 
 namespace Quotes.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    [Route("api/[controller]")]
+    public class QuotesController : Controller
     {
-        // GET api/values
+        public static List<QuoteModel> QuotesList = new List<QuoteModel>()
+        {
+            new QuoteModel()
+            {
+                Id = 1,
+                Author = "John Lennon",
+                Quote = "Life is what happens when you’re busy making other plans.",
+                Category = "Life",
+                CreationTime = DateTime.Now.AddMinutes(-50)
+            },
+            new QuoteModel()
+            {
+                Id = 2,
+                Author = "Unknow",
+                Quote = "Get busy living or get busy dying.",
+                Category = "Life",
+                CreationTime = DateTime.Now.AddHours(-1)
+
+            },
+            new QuoteModel()
+            {
+                Id = 3,
+                Author = "Harold Abelson",
+                Quote = "Programs must be written for people to read, and only incidentally for machines to execute.",
+                Category = "Programmers",
+                CreationTime = DateTime.Now
+            }
+        };
+
+        // GET api/all quotes
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [Route("GetAllQuotes")]
+        public IActionResult GetAllQuotes()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(QuotesList);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET api/quotes by category
+        [HttpGet]
+        [Route("GetAllQuotesByCategory")]
+        public IActionResult GetAllQuotesByCategory(string category)
         {
-            return "value";
+            return Ok(QuotesList.Where(p => p.Category.ToLower().Contains(category.ToLower())));
         }
 
-        // POST api/values
+        // GET api/random quote
+        [HttpGet]
+        [Route("GetRandomQuote")]
+        public IActionResult GetRandomQuote()
+        {
+            var quote = QuotesList[new Random().Next(QuotesList.Count)];
+            return Ok(quote);
+        }
+
+        // CRUD api/add quote
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("AddQuote")]        
+        public IActionResult AddQuote([FromBody] QuoteModel quote)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var maxId = QuotesList.Select(p => p.Id).Max();
+            
+            maxId++; 
+            quote.Id = maxId;
+            quote.CreationTime = DateTime.Now;
+            QuotesList.Add(quote);
+            
+            return Ok(new { massage = "Successfully added" });
+        }
+        
+        // CRUD api/delete quote by id
+        [Route("DeleteQuote/{id}")]
+        [HttpDelete]
+        public IActionResult DeleteQuote(int id)
+        {
+            if (QuotesList.Remove(QuotesList.Find(p => p.Id == id)))
+            {
+                return Ok(new { massage = "Successfully removed!" });
+            }
+            else
+            {
+                return BadRequest(new { massage = "Quote not found!" });
+            }
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // CRUD api/edit quote by id
+        [Route("EditQuote/{id}")]
+        [HttpPut]
+        public IActionResult EditQuote([FromRoute] int id, [FromBody] QuoteModel quote)
         {
+            bool isModified = false;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            QuotesList.ForEach(p =>
+            {
+                if (p.Id == id)
+                {
+                    p.Quote = quote.Quote;
+                    p.Author = quote.Author;
+                    p.Category = quote.Category;
+                    isModified = true;
+                }
+            });
+
+            if (isModified)
+            {
+                return Ok(new { massage = "Successfully updated!" });
+            }
+            else
+            {
+                return BadRequest(new { massage = "Not found" });
+            }
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
---
+++ Quotes/Models/QuoteModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Quotes.Models
+{
+    public class QuoteModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Quote { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
